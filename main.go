b8322df0174f91877fc5539c2102a6dd5c942778
Andrej <andrejkoman@hotmail.com>
2026-05-23 11:13:12 +0200

    intial file saving implementation

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, Arch Linux user! Go is working perfectly.")
-	test()
 }
 
-func test() {
+func SaveData(path string, data []byte) error {
+	tmp := fmt.Sprintf("%s.tmp.%d", path, rand.Int())
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o664)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		fp.Close()
+		if err != nil {
+			os.Remove(tmp)
+		}
+	}()
+
+	if _, err = fp.Write(data); err != nil {
+		return err
+	}
+
+	if err = fp.Sync(); err != nil {
+		return err
+	}
+
+	err = os.Rename(tmp, path)
+	return err
 }
